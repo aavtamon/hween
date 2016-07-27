ee4f650a5eb95f0b33edc04ee8458c87b842f965
aavtamon <anton.avtamonov@gmail.com>
2016-07-26 23:25:52 -0400

    corrected server implementation to properly handle the response

--- controller/src/com/piztec/hween/controller/ControlServer.java
+++ controller/src/com/piztec/hween/controller/ControlServer.java
@@ -2,6 +2,7 @@ package com.piztec.hween.controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -33,8 +34,10 @@ public class ControlServer {
 					ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
 					
 					while (!isInterrupted()) {
-						System.err.println("Waiting for the server to be accepted");
-						handleSocketConnection(serverSocket.accept());
+						String message = handleSocketConnection(serverSocket.accept());
+						if (message != null) {
+							handleMessage(message);
+						}
 					}
 					
 					serverSocket.close();
@@ -52,20 +55,59 @@ public class ControlServer {
 	}
 	
 	
-	private void handleSocketConnection(Socket clientSocket) throws IOException {
-		System.err.println("Started processing");
+	private String handleSocketConnection(Socket clientSocket) throws IOException {
 	    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-	    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));				
-		
-        StringBuilder responseText = new StringBuilder();
-        String line;
-        while ((line = in.readLine()) != null) {
-            responseText.append(line);
+	    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	    
+	    String result = null;
+	    
+        String line = in.readLine();
+        if (line.startsWith("OPTIONS ")) {
+        	generateResponse(out, null);
+        } else if (line.startsWith("PUT ")) {
+        	int contentLength = 0;
+            while ((line = in.readLine()) != null && !line.isEmpty()) {
+            	if (line.startsWith("Content-Length: ")) {
+            		contentLength = Integer.valueOf(line.substring("Content-Length: ".length()));
+            	}
+            }
+        	
+            if (contentLength > 0) {
+            	char[] content = new char[contentLength];
+            	in.read(content, 0, contentLength);
+            	
+            	result = String.valueOf(content);
+            }
+            
+            generateResponse(out, null);
         }
-        in.close();
         
-        System.out.println("Server: " + responseText);
+        in.close();
+        out.close();
 	    
         clientSocket.close();
+        
+        return result;
+	}
+	
+	private void generateResponse(final PrintWriter output, final String content) {
+		output.println("HTTP/1.1 200 OK");
+		output.println("Server: Device Controller");
+		output.println("Access-Control-Allow-Headers: origin, content-type, accept, authorization, token, location");
+		output.println("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD");
+		output.println("Access-Control-Allow-Origin: *");
+		if (content != null) {
+			output.println("Content-Length: " + content.getBytes().length);
+		}
+		output.println("Date: Wed, 27 Jul 2016 02:31:24 GMT");
+		if (content != null) {
+			output.println();
+			output.println(content);
+		}
+	}
+	
+	
+	private void handleMessage(final String message) {
+		System.out.println("Handling message: " + message);
 	}
 }
