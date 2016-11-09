cefd3288d8f15b83edf908782f454aa2a93bb8dd
aavtamon <anton.avtamonov@gmail.com>
2016-11-08 23:03:56 -0500

    fixed image creation script

--- create_image.sh
+++ create_image.sh
@@ -8,11 +8,14 @@ fi
 
 mkdir tmp
 cp image/controller.jar tmp
-cp image/controller.properties tmp
+cp image/system.properties tmp
 cp image/install.sh tmp
+cp image/code_upgrade.sh tmp
+cp image/run_controller.sh tmp
+cp image/common_defs.sh tmp
 cp -r image/lib tmp
 cp -r image/system tmp
-echo "{ \"version\": \"$1\"} " > tmp/version.json
+echo "version: $1" > tmp/version.properties
 
 rm image.tar
 cd tmp
